31c753ae39e76b0559b5a5103243f963e2db2cfe
HUAN <1523755788@qq.com>
2022-11-24 19:35:12 +0800

    first commit

---
+++ src/main/java/com/huan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.huan.git;
+
+/**
+ * @author HUAN
+ * @create 2022-11-24 19:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
